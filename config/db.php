81d9ba831a7708d4b4df5fbc500773f4a471beab
Sally <sally4388you@126.com>
2018-06-21 14:35:31 -0700

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=mjtool',
-    'username' => 'root',
-    'password' => '199551',
-    'charset' => 'utf8',
-];
